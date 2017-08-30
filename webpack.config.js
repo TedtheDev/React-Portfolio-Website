417781daed21300f2225cf24435e31417fdccf09
TedtheDev <tylerjedmunds@gmail.com>
2017-08-30 00:15:48 -0500

    set up directories and hello world but having some issues. it's bed time

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+const path = require('path');
+
+module.exports = {
+  entry: {
+    bundle: './src/index.js'
+  },
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'bundle.js',
+    publicPath: 'build/'
+
+  },
+  module: {
+    rules: [
+      {
+        use: 'babel-loader',
+        test: /\.js$/,
+        exclude: /node_modules/
+      }
+    ]
+  }
+}
