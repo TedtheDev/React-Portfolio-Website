09e8fb936e9dda2b225d866e721931f77eb96cca
TedtheDev <tylerjedmunds@gmail.com>
2017-09-03 18:23:46 -0500

    hello world app with css grid and styled components

--- src/index.js
+++ src/index.js
@@ -1,12 +1,25 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import styled from 'styled-components';
 
+const AppStyled = styled.div`
+  background: white;
+  width: 100%;
+  height: 100%;
+  display: grid;
+  grid-template-columns: 1fr 6fr 1fr;
+  grid-template-rows: 1fr 9fr 1fr;
+  grid-template-areas:
+    "header header header"
+    ". app ."
+    "footer footer footer"
+`
 class App extends Component {
   render() {
     return (
-      <div>
+      <AppStyled>
         Hello from App
-      </div>
+      </AppStyled>
     )
   }
 }
