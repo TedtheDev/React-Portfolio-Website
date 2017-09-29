67ee4219983b77dfb6debf5556b649d39f91b537
TedtheDev <tylerjedmunds@gmail.com>
2017-09-28 22:52:47 -0500

    added contact info

--- src/components/contact.js
+++ src/components/contact.js
@@ -1,12 +1,37 @@
 import React from 'react';
 import styled from 'styled-components';
 
+const ContactDiv = styled.div`
+  display: grid;
+  grid-template-columns: 1fr;
+  grid-template-rows: auto;
+`;
+
+const ContactItemDiv = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  grid-auto-rows: auto;
+  justify-items: center;
+  align-items: center;
+`;
+
+const HorizontalLine = styled.hr`
+  width: 100%;
+`;
+
 const Contact = (props) => {
   return(
-    <div>
-      <i className="fa fa-linkedin" aria-hidden="true"></i>
-      <i className="fa fa-envelope-o" aria-hidden="true"></i>
-    </div>
+    <ContactDiv>
+      <ContactItemDiv>
+        <a href="https://www.linkedin.com/in/tyler-edmunds" target="_blank" rel="noreferrer noopener"><i className="fa fa-linkedin fa-3x" aria-hidden="true"></i></a>
+        <div style={{fontSize:"1.2rem"}}>Tyler Edmunds</div>
+      </ContactItemDiv>
+      <HorizontalLine />
+      <ContactItemDiv>
+        <a href="mailto:tylerjedmunds@gmail.com?subject=Contacting%20You%20From%20Your%20Website" rel="noreferrer noopener"><i className="fa fa-envelope-o fa-3x" aria-hidden="true"></i></a>
+        <div style={{fontSize:"1.2rem"}}>tylerjedmunds@gmail.com</div>
+      </ContactItemDiv>
+    </ContactDiv>
   )
 };
 
