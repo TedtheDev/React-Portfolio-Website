fe9018017d8ef7cbd199f11fbb9ea79586ded49a
TedtheDev <tylerjedmunds@gmail.com>
2017-10-08 22:10:54 -0500

    aligned links to projects; added line between projects

--- src/components/portfolio_project.js
+++ src/components/portfolio_project.js
@@ -3,20 +3,32 @@ import styled from 'styled-components';
 import PropTypes from 'prop-types';
 
 const ProjectDiv = styled.div`
+  display: grid;
+  grid-template-columns: 1fr;
+  grid-template-rows: auto;
+  margin-bottom: 10%;
+  &:not(:last-child)::after {
+    position: relative;
+    content: "";
+    width: 100%;
+    height: 2px;
+    background: #6CCFF6;
+  }
 `;
 
 const ProjectHeading = styled.div`
-    display: grid;
-    grid-template-rows: 1fr;
-    grid-template-columns: 2fr .5fr .5fr 1fr;
+    display: flex;
     margin-bottom: 2%;
+    & a {
+      margin-right: 2%;
+    }
 `;
 
 const Project = (props) => {
   return (
     <ProjectDiv>
       <ProjectHeading>
-        <div style={{fontSize:"1.3rem"}}><strong>{props.project.projectName}</strong></div>
+        <div style={{fontSize:"1.3rem",marginRight:"2%"}}><strong>{props.project.projectName}</strong></div>
         <a href={props.project.liveLink} target="_blank" rel="noopener noreferrer"><i className="fa fa-desktop fa-2x" aria-hidden="true"></i></a>
         <a href={props.project.github} target="_blank" rel="noopener noreferrer"><i className="fa fa-github fa-2x" aria-hidden="true"></i></a>
       </ProjectHeading>
