6f991abb40ff63d4723ba98e17b4e466ac0ff4e7
TedtheDev <tylerjedmunds@gmail.com>
2018-08-06 22:49:25 -0500

    refactor portfolio section

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -4,13 +4,10 @@ import { portfolioData } from '../../data/portfolio';
 import Project from '../components/PortfolioProject';
 
 const PortfolioDiv = styled.section`
-  width: 100%;
-  height: 100%;
   grid-area: app;
   display: grid;
   grid-template-columns: 1fr;
   grid-template-rows: auto;
-  margin: 3% 0;
 `;
 
 const Portfolio = (props) => {
