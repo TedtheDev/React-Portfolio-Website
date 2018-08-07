0ddae43a7b578b77a1d8e5ea0e7b7e7cca727dd6
TedtheDev <tylerjedmunds@gmail.com>
2018-08-06 22:46:22 -0500

    added api to send emails

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
+require('dotenv').config();
 const path = require('path');
 const app = require('express')();
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const mailgun = require('./controllers/mailgun');
+
+if(process.env.NODE_ENV !== 'production') {
+  app.use(cors());
+}
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 app.get('/', (req,res) => {
   res.sendFile(path.resolve(__dirname, 'index.html'));
@@ -8,6 +18,8 @@ app.get('/', (req,res) => {
 app.get('/build/bundle.js', (req,res) => {
   res.sendFile(path.resolve(__dirname, './build/bundle.js'));
 })
+
+app.post('/api/message', mailgun)
 
 app.get('*', (req,res) => {
   res.redirect('/');
